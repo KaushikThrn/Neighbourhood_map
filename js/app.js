34e23b37cd00b2f2c0bbfc1a3e639265f9dba3de
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2018-05-26 21:53:50 +0530

    added the list using knockout js

--- js/app.js
+++ js/app.js
@@ -5,4 +5,25 @@ function initMap() {
     center: {lat: 41.385064, lng: 2.173403},
     zoom: 8
      })
+
+    ko.applyBindings(new ViewModel());
  };
+
+var addMarker=function(location){
+
+	var marker = new google.maps.Marker({
+    position: location.latlong,
+    map: map,
+    title: location.title
+  });
+};
+
+var ViewModel=function(){
+    var self=this;
+    locations.forEach(function(location){addMarker(location)});
+    self.locationsarray = ko.observableArray(locations);
+
+
+};
+
+
