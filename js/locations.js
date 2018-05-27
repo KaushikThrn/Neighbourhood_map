32e2b2c26bee5404507042253b5feb36e8fb291c
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2018-05-27 12:18:31 +0530

    added 5 locations in barcelona

---
+++ js/locations.js
@@ -0,0 +1,37 @@
+var locations=[
+    {
+        title: 'Camp Nou',
+        latlong: {
+            lat: 41.380896,
+            lng: 2.1228198
+        }
+    },
+    {  
+        title: 'Temple of the Sacred Heart',
+        latlong: {
+            lat: 41.422075,
+            lng: 2.118787
+        }
+    },
+    {
+        title: 'La Sagrada Familia',
+        latlong: {
+            lat: 41.403630,
+            lng: 2.174356
+        }
+    },
+    {
+        title: 'Casa Batlló',
+        latlong: {
+            lat: 41.391638,
+            lng: 2.16477
+        }
+    },
+    {
+        title: 'Casa Vicens Gaudí',
+        latlong: {
+            lat: 41.403498,
+            lng: 2.150645
+        }
+    },
+]
