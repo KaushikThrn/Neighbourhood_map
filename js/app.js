b51fdf8bd9d124f227cbb4f803528ec9079d57ae
Kaushik Thiyagarajan -X (kauthiya - MINDTREE LIMITED at Cisco) <kauthiya@cisco.com>
2018-05-29 12:42:38 +0530

    working, must deal with the ajax call delay later

--- js/app.js
+++ js/app.js
@@ -2,26 +2,7 @@ var map;
 var infoWindows=[];
 var markers=[];
 var blurb;
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
+var contentString = "Loading Pls wait"
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -40,12 +21,10 @@ var addMarker=function(location){
     map: map,
     title: location.title
   });
-
-  infowindow=createInfoWindow();
    
-
   marker.addListener('click', function() {
   	closeAllInfoWIndows();
+    console.log("sending from event listener "+marker.title)
     infowindow=createInfoWindow(marker.title);
     infowindow.open(map, marker);
   });
@@ -58,6 +37,8 @@ var closeAllInfoWIndows=function(){
 }
 
 var createInfoWindow=function(title){
+  var infowindow;
+  console.log("create infowindow "+title)
   try {
     var search=title.replace(" ","_")
 }
